bb3e7f882bdb3d721e1bdec33687af27b7d39779
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-30 14:55:36 +0300

    refactor: remove unnecessary code

--- src/common/interfaces/CRUD.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface CRUD<T, C, P> {
-  list: (limit: number, page: number) => Promise<T[]>;
-  create: (resource: C) => Promise<T>;
-  putById: (id: string, resource: P) => Promise<T | null>;
-  readById: (id: string) => Promise<T | null>;
-  deleteById: (id: string) => Promise<boolean>;
-}
