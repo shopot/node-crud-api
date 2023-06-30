f81c7a55d95e0c43077c18bff2e16ac07f4736a9
Sapronov Konstantin <ks.costas@gmail.com>
2023-06-30 12:02:27 +0300

    feat: add crud interface

--- src/common/interfaces/CRUD.interface.ts
+++ src/common/interfaces/CRUD.interface.ts
@@ -1,6 +1,6 @@
 export interface CRUD<T, C, P> {
   list: (limit: number, page: number) => Promise<T[]>;
-  create: (resource: C) => Promise<string>;
+  create: (resource: C) => Promise<T>;
   putById: (id: string, resource: P) => Promise<T | null>;
   readById: (id: string) => Promise<T | null>;
   deleteById: (id: string) => Promise<boolean>;
