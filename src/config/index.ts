8e2525e64c173b3f840cd8f8b52866508c04d99f
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-02 18:41:13 +0300

    refactor: change default port

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ export const APP_USERS_ENDPOINT = '/api/users';
 
 export const APP_ENDPOINTS = [APP_USERS_ENDPOINT];
 
-export const APP_PORT = process.env.PORT || 3001;
+export const APP_PORT = process.env.PORT || 4000;
 
 export const API_HOSTNAME = 'localhost';
