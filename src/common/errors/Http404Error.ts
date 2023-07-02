4958592023c9c85947f1793ee1ad41bb7a6dca60
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-02 18:12:20 +0300

    fix: rename class

--- src/common/errors/Http404Error.ts
+++ src/common/errors/Http404Error.ts
@@ -1,7 +1,7 @@
 import { BaseError } from './BaseError';
 import { HttpStatusCode } from '../HttpStatusCode';
 
-export class HTTP404Error extends BaseError {
+export class Http404Error extends BaseError {
   constructor(description = 'Not found') {
     super('NOT FOUND', HttpStatusCode.NOT_FOUND, description, true);
   }
