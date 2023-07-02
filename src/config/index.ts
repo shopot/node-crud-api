8b8c000625cd5ba09f205d59dfc3b042f5da31b6
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-02 18:18:30 +0300

    feat: add hostname

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-import 'dotenv/config';
-
 export const APP_USERS_ENDPOINT = '/api/users';
 
 export const APP_ENDPOINTS = [APP_USERS_ENDPOINT];
 
 export const APP_PORT = process.env.PORT || 3001;
+
+export const API_HOSTNAME = 'localhost';
