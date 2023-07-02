d21970e703de2bb9bd7aaf67466f0bd4fc967810
Sapronov Konstantin <ks.costas@gmail.com>
2023-07-02 18:22:40 +0300

    feat: add errors guard

--- src/handler.ts
+++ src/handler.ts
@@ -3,7 +3,6 @@ import { IncomingMessage, ServerResponse } from 'http';
 import { createEndpointKey } from './util/createEndpointKey';
 import { APP_ENDPOINTS } from './config';
 import { routes } from './routes/routes';
-import { errorHandler } from './common/ErrorHandler';
 import { BaseError } from './common/errors/BaseError';
 import { DEFAULT_HEADER } from './common/constants';
 import { HttpStatusCode } from './common/HttpStatusCode';
@@ -31,25 +30,25 @@ const handlerError = (
     req: IncomingMessage;
   }
 ) => {
+  const writeErrorResponse = (httpCode: number, message: string) => {
+    response.writeHead(httpCode, DEFAULT_HEADER);
+
+    response.write(
+      JSON.stringify({
+        error: message,
+      })
+    );
+  };
+
   return (error: Error) => {
-    errorHandler.handleError(error);
+    if (BaseError.isTrustedError(error)) {
+      writeErrorResponse((error as BaseError).httpCode, error.message);
+    } else if (BaseError.isString(error)) {
+      const data = BaseError.createResponse(String(error));
 
-    if (errorHandler.isTrustedError(error)) {
-      response.writeHead((error as BaseError).httpCode, DEFAULT_HEADER);
-
-      response.write(
-        JSON.stringify({
-          error: error.message,
-        })
-      );
+      writeErrorResponse(data.httpCode, data.message);
     } else {
-      response.writeHead(HttpStatusCode.INTERNAL_SERVER, DEFAULT_HEADER);
-
-      response.write(
-        JSON.stringify({
-          error: ErrorMessage.INTERNAL_SERVER_ERROR,
-        })
-      );
+      writeErrorResponse(HttpStatusCode.INTERNAL_SERVER, ErrorMessage.INTERNAL_SERVER_ERROR);
     }
 
     response.end();
